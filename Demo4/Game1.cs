048ea5db5d607985cdce44062f959831efc2c057
Thomas Kent <the1krutz@gmail.com>
2020-07-14 21:12:18 -0700

    got the first section of Tutorial 4 working
    The pre-installed monogame-pipeline-tool wasnt able to import mp3 becuase it was looking in the wrong place for ffmpeg and ffprobe
    
    Long story short, Ive switched to building Monogame from source, got a more recent C# template, and have to run the pipeline tool as the MonoGame Content Builder Editor (mgcbe) from the terminal BUT its totally capable of importing audio files now.
    
    Worth it.

--- Demo4/Game1.cs
+++ Demo4/Game1.cs
@@ -1,52 +1,56 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
-namespace Demo4
-{
-    public class Game1 : Game
-    {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+namespace Demo4 {
+  public class Game1 : Game {
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    Song song;
 
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-        }
-
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
-        }
+    public Game1() {
+      _graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+      IsMouseVisible = true;
     }
+
+    protected override void Initialize() {
+      base.Initialize();
+    }
+
+    protected override void LoadContent() {
+      _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      song = Content.Load<Song>("sample_mp3");
+      MediaPlayer.Play(song);
+      // uncomment the following line to loop the song
+      //MediaPlayer.IsRepeating = true;
+      MediaPlayer.MediaStateChanged += MediaPlayer_MediaStateChanged;
+    }
+
+    protected override void UnloadContent() {
+      Content.Unload();
+      base.UnloadContent();
+    }
+
+    void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e) {
+      // 0.0f is silent, 0.1f is full volume
+      MediaPlayer.Volume -= 0.1f;
+      MediaPlayer.Play(song);
+    }
+
+
+    protected override void Update(GameTime gameTime) {
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        Exit();
+
+      base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime) {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+      base.Draw(gameTime);
+    }
+  }
 }
