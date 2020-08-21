29f419ea58b41de77c7c52b79f521805a2fd59c3
Thomas Kent <the1krutz@gmail.com>
2020-08-21 14:35:30 -0700

    add test project for Nez
    planning to follow the Nez video tutorial series

---
+++ Test1_Nez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test1_Nez
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
