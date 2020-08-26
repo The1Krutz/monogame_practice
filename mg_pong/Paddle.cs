5248af7844c1416ad51bcc46b7319b1fee2ad4f2
Thomas Kent <the1krutz@gmail.com>
2020-08-26 15:33:59 -0700

    temp add pong paddle class

---
+++ mg_pong/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+
+namespace mg_pong {
+  public class Paddle {
+    public Vector2 Position { get; set; }
+    public Vector2 Velocity { get; set; }
+
+    private readonly double _maxSpeed;
+
+    public Paddle(Vector2 position, Vector2 velocity) {
+      Position = position;
+      Velocity = velocity;
+
+      _maxSpeed = 5;
+    }
+
+    public void UpdatePosition() {
+      Position += Velocity;
+
+    }
+
+  }
+}
