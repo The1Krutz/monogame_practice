4a728c2b2059bbe2d990e4da08383b0a17921732
Thomas Kent <the1krutz@gmail.com>
2020-08-26 22:29:44 -0700

    it's Pong baby!
    It's not pretty Pong, and it doesn't have scores, but it works. It's playable

---
+++ mg_pong/BouncyBoi.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace mg_pong {
+  public class BouncyBoi {
+    public Vector2 Velocity;
+    public Texture2D Sprite;
+    public Rectangle BoundingBox; // CRITICAL: these coordinates are int pixel values, and won't change at all if the update is less than one pixel.
+    public readonly float MaxSpeed; // max speed in pixels per second
+
+    public readonly Keys upKey;
+    public readonly Keys downKey;
+
+    private void LogInfo(string additionalInfo = null) {
+      System.Console.WriteLine($"Bounding box: {BoundingBox.ToString()}\nVelocity: {Velocity}");
+      if (additionalInfo != null) {
+        System.Console.WriteLine(additionalInfo);
+      }
+    }
+
+    public BouncyBoi(Vector2 velocity, Texture2D sprite, Rectangle boundingbox, Keys upkey = Keys.None, Keys downkey = Keys.None) {
+      Velocity = velocity;
+      Sprite = sprite;
+      BoundingBox = boundingbox;
+      upKey = upkey;
+      downKey = downkey;
+
+      MaxSpeed = 200;
+    }
+
+    public void Update(KeyboardState keyMap, GameTime gameTime) {
+      HandleInput(keyMap);
+
+      if (Velocity.Length() > MaxSpeed) {
+        Velocity.Normalize();
+        Velocity *= MaxSpeed;
+        LogInfo("Speed Limit!");
+      }
+
+      BoundingBox.Offset(Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds);
+      //LogInfo();
+    }
+
+    public void BounceUp() {
+      Velocity = Vector2.Reflect(Velocity, new Vector2(0, -1));
+    }
+
+    public void BounceDown() {
+      Velocity = Vector2.Reflect(Velocity, new Vector2(0, 1));
+    }
+
+    public void BounceLeft() {
+      Velocity = Vector2.Reflect(Velocity, new Vector2(-1, 0));
+    }
+
+    public void BounceRight() {
+      Velocity = Vector2.Reflect(Velocity, new Vector2(1, 0));
+    }
+
+    private void HandleInput(KeyboardState keyMap) {
+      if (upKey == Keys.None || downKey == Keys.None) {
+        return;
+      }
+
+      if (keyMap.IsKeyDown(upKey)) {
+        Velocity = new Vector2(0, MaxSpeed * -1);
+      } else if (keyMap.IsKeyDown(downKey)) {
+        Velocity = new Vector2(0, MaxSpeed);
+      } else {
+        Velocity = Vector2.Zero;
+      }
+    }
+  }
+}
--- mg_pong/Game1.cs
+++ mg_pong/Game1.cs
@@ -2,20 +2,45 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+
 namespace mg_pong {
   public class Game1 : Game {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private SpriteFont _kenneyBold;
+    private Texture2D _headerBar;
+
+    private Rectangle playableArea;
+
+    private BouncyBoi leftPaddle;
+    private BouncyBoi rightPaddle;
+    private BouncyBoi ball;
 
     public Game1() {
       _graphics = new GraphicsDeviceManager(this);
       Content.RootDirectory = "Content";
       IsMouseVisible = true;
+
+      playableArea = new Rectangle(0, 50, 800, 550);
     }
 
     protected override void Initialize() {
-      // TODO: Add your initialization logic here
+      // set window size
+      _graphics.PreferredBackBufferHeight = playableArea.Height + 50;
+      _graphics.PreferredBackBufferWidth = playableArea.Width;
+      _graphics.ApplyChanges();
+      Window.AllowUserResizing = true;
+
+      // header bar
+      _headerBar = new Texture2D(GraphicsDevice, playableArea.Width, 50);
+      Color[] colorData = new Color[playableArea.Width * 50];
+      System.Array.Fill(colorData, Color.Red);
+      _headerBar.SetData(colorData);
+
+      // add paddles and ball
+      leftPaddle = new BouncyBoi(Vector2.Zero, Content.Load<Texture2D>("minus"), new Rectangle(playableArea.Left + 50, playableArea.Top + 50, 10, 100), Keys.W, Keys.S);
+      rightPaddle = new BouncyBoi(Vector2.Zero, Content.Load<Texture2D>("minus"), new Rectangle(playableArea.Right - 60, playableArea.Top + 50, 10, 100), Keys.Up, Keys.Down);
+      ball = new BouncyBoi(new Vector2(1000), Content.Load<Texture2D>("stop"), new Rectangle(200, 200, 30, 30));
 
       base.Initialize();
     }
@@ -26,8 +51,29 @@ namespace mg_pong {
     }
 
     protected override void Update(GameTime gameTime) {
-      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      KeyboardState state = Keyboard.GetState();
+
+      if (state.IsKeyDown(Keys.Escape))
         Exit();
+
+
+      leftPaddle.Update(state, gameTime);
+      rightPaddle.Update(state, gameTime);
+      ball.Update(state, gameTime);
+
+      if (ball.BoundingBox.Bottom >= playableArea.Bottom) {
+        ball.BounceUp();
+      }
+      if (ball.BoundingBox.Top <= playableArea.Top) {
+        ball.BounceDown();
+      }
+      if (ball.BoundingBox.Right >= rightPaddle.BoundingBox.Left) {
+        ball.BounceLeft();
+      }
+      if (ball.BoundingBox.Left <= leftPaddle.BoundingBox.Right) {
+        ball.BounceRight();
+      }
+
 
       base.Update(gameTime);
     }
@@ -36,10 +82,19 @@ namespace mg_pong {
       GraphicsDevice.Clear(Color.Black);
 
       _spriteBatch.Begin();
-      _spriteBatch.DrawString(_kenneyBold, "WhiteSmoke", new Vector2(32, 32), Color.WhiteSmoke);
+      _spriteBatch.Draw(_headerBar, new Rectangle(Point.Zero, new Point(800, 50)), Color.Gray);
+      _spriteBatch.DrawString(_kenneyBold, "Header Bar", new Vector2(50, 15), Color.Black);
+      _spriteBatch.Draw(leftPaddle.Sprite, leftPaddle.BoundingBox, Color.White);
+      _spriteBatch.Draw(rightPaddle.Sprite, rightPaddle.BoundingBox, Color.White);
+      _spriteBatch.Draw(ball.Sprite, ball.BoundingBox, Color.White);
       _spriteBatch.End();
 
       base.Draw(gameTime);
+    }
+
+    protected override void UnloadContent() {
+      Content.Unload();
+      base.UnloadContent();
     }
   }
 }
